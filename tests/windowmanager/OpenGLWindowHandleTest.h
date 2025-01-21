187595a6c5d00c5eb54132dca8f00f3dab9b23c9
Henry <henryisaway@gmail.com>
2025-01-21 00:57:11 -0300

    chore: removed tests from repository

--- tests/windowmanager/OpenGLWindowHandleTest.h
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "../../include/windowmanager/OpenGLWindowHandle.h"
-#include "../../include/core/config.h"
-
-class OpenGLWindowHandleTest : public ::testing::Test {
-protected:
-    void SetUp() override;
-    void TearDown() override;
-};
