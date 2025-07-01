34c2c5302459c51e3cfdd2f652ab7891cf658e7c
Henry <henryisaway@gmail.com>
2025-07-01 09:50:04 -0300

    fix: Fixed main window not being able to initialise.
    The program was wrongfuly requesting OpenGL version 3.5 instead of 3.3. Changing GLFW settings to request the correct version has fixed the issue.

--- src/renderer/OpenGLRenderer.cpp
+++ src/renderer/OpenGLRenderer.cpp
@@ -36,7 +36,7 @@ void OpenGLRenderer::loadGLFW() {
     glfwDefaultWindowHints();
     
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
